36e032a3d700c6504a6a5ab1581c6ee7c09a9a2a
Russell Power <russell.power@gmail.com>
2015-05-05 11:40:27 +0900

    Working..

--- tool/vdbe-functions.py
+++ tool/vdbe-functions.py
@@ -5,5 +5,36 @@ import re
 import sys
 
 def main():
-  for line in sys.stdin.readlines():
+  functions = collections.OrderedDict()
+  fn_name, fn_code = None, None
+  
+  lines = list(sys.stdin.readlines())
+  
+  while lines:
+    line = lines.pop(0)
+    print line,
+    match = re.search(r'case OP_(\w+)', line)
+    if not match:
+      continue
     
+    fn_name = match.group(1)
+    fn_code = []
+    while 1:
+      line = lines.pop(0)
+      if '/* Opcode:' in line:
+	print line
+        break
+      
+      print line,
+      fn_code.append(line)
+    
+    functions[fn_name] = fn_code
+   
+  for fn_name, fn_code in functions.items():
+    print 'void bytcode_fn_%s(ExecState* state) {' % fn_name
+    print ''.join(fn_code)
+    print '}'
+    print '\n\n\n' 
+      
+      
+main()
